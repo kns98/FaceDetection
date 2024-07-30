06a141a6806a7c709f1383bfe3e1c12c5da5b1aa
kevin <kevin@DESKTOP-GS46PVC>
2024-07-30 14:49:48 -0700

    test

--- FaceDetetion/Program.cs
+++ FaceDetetion/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Threading;
@@ -48,13 +49,9 @@ class EdgeDetectionApp
             using (Bitmap inputImage = new Bitmap(file))
             {
                 Bitmap edgeImage = SobelEdgeDetection(inputImage);
-                float harrisThreshold = 0.01f;
-                float faceClusterThreshold = 0.1f;
-                var corners = HarrisCorners(edgeImage, harrisThreshold);
-                var faces = DetectFaces(corners, faceClusterThreshold * edgeImage.Width);
-                Bitmap annotatedImage = AnnotateExpression(edgeImage, faces);
-                annotatedImage.Save(outputFilePath);
-                Console.WriteLine($"Edge and expression detection completed for {file}. Result saved to {outputFilePath}");
+                Bitmap harrisImage = DetectHarrisCorners(edgeImage);
+                harrisImage.Save(outputFilePath);
+                Console.WriteLine($"Edge and corner detection completed for {file}. Result saved to {outputFilePath}");
             }
         }
         catch (Exception ex)
@@ -75,7 +72,7 @@ class EdgeDetectionApp
             for (int x = 0; x < image.Width; x++)
             {
                 Color originalColor = image.GetPixel(x, y);
-                int grayScale = (int)((originalColor.R * 0.3) + (originalColor.G * 0.59) + (originalColor.B * 0.11));
+                int grayScale = (int)(originalColor.R * 0.299 + originalColor.G * 0.587 + originalColor.B * 0.114);
                 Color grayColor = Color.FromArgb(grayScale, grayScale, grayScale);
                 grayscale.SetPixel(x, y, grayColor);
             }
@@ -117,136 +114,107 @@ class EdgeDetectionApp
         return edgeImage;
     }
 
-    private static List<Point> HarrisCorners(Bitmap image, float threshold)
+    private static Bitmap DetectHarrisCorners(Bitmap image)
     {
-        int width = image.Width;
-        int height = image.Height;
-        List<Point> corners = new List<Point>();
+        Bitmap grayscaleImage = ConvertToGrayscale(image);
+        (Bitmap Ix, Bitmap Iy) = CalculateSpatialDerivatives(grayscaleImage);
+        Bitmap responseImage = CalculateHarrisResponse(grayscaleImage, Ix, Iy);
+        Bitmap corners = NonMaximumSuppression(responseImage);
 
-        Func<int, int, int> getPixel = (x, y) =>
-        {
-            if (x < 0 || y < 0 || x >= width || y >= height) return 0;
-            return image.GetPixel(x, y).R;
-        };
-
-        for (int y = 1; y < height - 2; y++)
-        {
-            for (int x = 1; x < width - 2; x++)
-            {
-                int ix = (getPixel(x + 1, y) - getPixel(x - 1, y)) / 2;
-                int iy = (getPixel(x, y + 1) - getPixel(x, y - 1)) / 2;
-                int ixx = ix * ix;
-                int iyy = iy * iy;
-                int ixy = ix * iy;
-                int det = ixx * iyy - ixy * ixy;
-                int trace = ixx + iyy;
-                float r = det - 0.04f * (trace * trace);
-                if (r > threshold)
-                {
-                    corners.Add(new Point(x, y));
-                }
-            }
-        }
         return corners;
     }
 
-    private static List<List<Point>> DetectFaces(List<Point> corners, float threshold)
+    private static (Bitmap, Bitmap) CalculateSpatialDerivatives(Bitmap image)
     {
-        List<List<Point>> clusters = new List<List<Point>>();
+        Bitmap Ix = new Bitmap(image.Width, image.Height);
+        Bitmap Iy = new Bitmap(image.Width, image.Height);
 
-        Func<Point, Point, double> distance = (p1, p2) =>
+        for (int y = 1; y < image.Height - 1; y++)
         {
-            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
-        };
-
-        foreach (var corner in corners)
-        {
-            bool added = false;
-            foreach (var cluster in clusters)
+            for (int x = 1; x < image.Width - 1; x++)
             {
-                if (cluster.Exists(c => distance(c, corner) < threshold))
-                {
-                    cluster.Add(corner);
-                    added = true;
-                }
-            }
+                int dx = (image.GetPixel(x + 1, y).R - image.GetPixel(x - 1, y).R) / 2;
+                int dy = (image.GetPixel(x, y + 1).R - image.GetPixel(x, y - 1).R) / 2;
 
-            if (!added)
-            {
-                List<Point> newCluster = new List<Point> { corner };
-                clusters.Add(newCluster);
+                Ix.SetPixel(x, y, Color.FromArgb(dx, dx, dx));
+                Iy.SetPixel(x, y, Color.FromArgb(dy, dy, dy));
             }
         }
 
-        return clusters.FindAll(cluster => cluster.Count > 10);
+        return (Ix, Iy);
     }
 
-    private static string DetectExpression(Bitmap image, List<Point> face)
+    private static Bitmap CalculateHarrisResponse(Bitmap image, Bitmap Ix, Bitmap Iy)
     {
-        int minX = int.MaxValue;
-        int minY = int.MaxValue;
-        int maxX = int.MinValue;
-        int maxY = int.MinValue;
+        Bitmap response = new Bitmap(image.Width, image.Height);
 
-        foreach (var point in face)
+        for (int y = 1; y < image.Height - 1; y++)
         {
-            if (point.X < minX) minX = point.X;
-            if (point.Y < minY) minY = point.Y;
-            if (point.X > maxX) maxX = point.X;
-            if (point.Y > maxY) maxY = point.Y;
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                double Ixx = 0, Iyy = 0, Ixy = 0;
+
+                for (int v = -1; v <= 1; v++)
+                {
+                    for (int u = -1; u <= 1; u++)
+                    {
+                        int ix = Ix.GetPixel(x + u, y + v).R;
+                        int iy = Iy.GetPixel(x + u, y + v).R;
+
+                        Ixx += ix * ix;
+                        Iyy += iy * iy;
+                        Ixy += ix * iy;
+                    }
+                }
+
+                double determinant = (Ixx * Iyy) - (Ixy * Ixy);
+                double trace = Ixx + Iyy;
+                double responseValue = determinant - 0.04 * trace * trace;
+
+                int r = Math.Max(0, Math.Min(255, (int)responseValue));
+                response.SetPixel(x, y, Color.FromArgb(r, r, r));
+            }
         }
 
-        Rectangle mouthRegion = new Rectangle(minX, minY + (maxY - minY) * 2 / 3, maxX - minX, (maxY - minY) / 3);
-        Rectangle eyeRegion = new Rectangle(minX, minY, maxX - minX, (maxY - minY) / 3);
-
-        Bitmap mouthImage = image.Clone(mouthRegion, image.PixelFormat);
-        Bitmap eyeImage = image.Clone(eyeRegion, image.PixelFormat);
-
-        Func<Bitmap, int> averageColor = (bmp) =>
-        {
-            int totalColor = 0;
-            for (int x = 0; x < bmp.Width; x++)
-            {
-                for (int y = 0; y < bmp.Height; y++)
-                {
-                    Color color = bmp.GetPixel(x, y);
-                    totalColor += (color.R + color.G + color.B) / 3;
-                }
-            }
-            return totalColor / (bmp.Width * bmp.Height);
-        };
-
-        int mouthIntensity = averageColor(mouthImage);
-        int eyeIntensity = averageColor(eyeImage);
-
-        int smileThreshold = 100;
-        int frownThreshold = 50;
-
-        if (mouthIntensity > smileThreshold) return "smile";
-        else if (mouthIntensity < frownThreshold) return "frown";
-        else return "neutral";
+        return response;
     }
 
-    private static Bitmap AnnotateExpression(Bitmap image, List<List<Point>> faces)
+    private static Bitmap NonMaximumSuppression(Bitmap response)
     {
-        Bitmap annotatedImage = new Bitmap(image);
-        using (Graphics graphics = Graphics.FromImage(annotatedImage))
-        {
-            foreach (var face in faces)
-            {
-                string expression = DetectExpression(image, face);
-                int minX = int.MaxValue;
-                int minY = int.MaxValue;
+        Bitmap corners = new Bitmap(response.Width, response.Height);
 
-                foreach (var point in face)
+        for (int y = 1; y < response.Height - 1; y++)
+        {
+            for (int x = 1; x < response.Width - 1; x++)
+            {
+                Color pixelColor = response.GetPixel(x, y);
+                bool isCorner = true;
+
+                for (int v = -1; v <= 1; v++)
                 {
-                    if (point.X < minX) minX = point.X;
-                    if (point.Y < minY) minY = point.Y;
+                    for (int u = -1; u <= 1; u++)
+                    {
+                        if (u == 0 && v == 0) continue;
+                        if (response.GetPixel(x + u, y + v).R > pixelColor.R)
+                        {
+                            isCorner = false;
+                            break;
+                        }
+                    }
+                    if (!isCorner) break;
                 }
 
-                graphics.DrawString(expression, new Font("Arial", 16.0f), Brushes.Red, minX, minY);
+                if (isCorner)
+                {
+                    corners.SetPixel(x, y, Color.Red);
+                }
+                else
+                {
+                    corners.SetPixel(x, y, Color.Black);
+                }
             }
         }
-        return annotatedImage;
+
+        return corners;
     }
 }
